db95c993d4981cced8db15127c04281e0a2bac7f
Hannah Hofmann <hhofmann@lcs.on.ca>
2017-11-23 21:42:28 -0500

    I did the put it all together.

--- ShowingMyUnderstanding.playground/Pages/Put it all together.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Put it all together.xcplaygroundpage/Contents.swift
@@ -21,7 +21,61 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
-// Replace this comment and add your code below...
+//hue
+let hue = random(from: 1, toButNotIncluding: 361)
+
+//background
+for i in stride(from: 0, to: 310, by: 10) {
+    let bright = i + 100
+    canvas.lineColor = Color.init(hue: hue, saturation: 100, brightness: bright, alpha: 100)
+    canvas.defaultLineWidth = 10
+    canvas.drawLine(fromX: i, fromY: 0, toX: i, toY: i+10)
+}
+
+// Make and abstarct looking thing
+
+//make the center somewhere off to the side
+canvas.translate(byX: 65, byY: -60)
+
+//number
+let number = random(from: 1, toButNotIncluding: 4)
+
+//he loop and colour
+for degree in stride(from: 0, to: 360, by: 1) {
+    let brightness = degree * 10
+    canvas.fillColor = Color.init(hue: hue, saturation: 100, brightness: brightness, alpha: 100)
+    
+    //conditionals
+    if number == 1 {
+    canvas.drawRectangle(centreX: 3, centreY: 10, width: 10, height: 10)
+    }; if number == 2 {
+        canvas.drawEllipse(centreX: 3, centreY: 10, width: 10, height: 10)
+    }
+    if number == 3 {canvas.drawRectangle(centreX: 5, centreY: 12, width: 10, height: 20)}
+    
+    //make it move weirdly
+        canvas.translate(byX: +20, byY: +20)
+    canvas.rotate(by: Degrees(degree))
+}
+
+canvas.borderColor = Color.white
+//colour
+for degree in stride(from: 0, to: 360, by: 1) {
+    let brightness = degree * 10
+    canvas.fillColor = Color.init(hue: hue, saturation: 100, brightness: brightness, alpha: 100)
+    
+    //conditionals
+    if number == 1 {
+        canvas.drawRectangle(centreX: 97, centreY: 90, width: 10, height: 10)
+    }; if number == 2 {
+        canvas.drawEllipse(centreX: 97, centreY: 90, width: 10, height: 10)
+    }
+    if number == 3 {canvas.drawRectangle(centreX: 95, centreY: 88, width: 10, height: 20)}
+    
+    //move
+    canvas.translate(byX: -20, byY: -20)
+    canvas.rotate(by: Degrees(degree))
+}
 
 
 /*:
