8c3eb94f9a3edf9869a5f7a57b9bf564b7dff26b
Hannah Hofmann <hhofmann@lcs.on.ca>
2017-11-25 11:16:41 -0500

    I added more to the conditionals example

--- ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
@@ -22,7 +22,7 @@ let canvas = Canvas(width: 300, height: 300)
 canvas.drawShapesWithBorders = false
 
 // Generate a random number
-let number = random(from: 0, toButNotIncluding: 2)
+let number = random(from: 0, toButNotIncluding: 3)
 
 //hue
 let hue = random(from: 0, toButNotIncluding: 361)
@@ -34,6 +34,9 @@ for c in stride(from: 15, to: 300, by: 30) {
         canvas.fillColor = Color(hue: hue, saturation: 80, brightness: brightness, alpha: 100)
         if number == 1 {
             canvas.drawEllipse(centreX: c, centreY: y, width: 20, height: 20)
+        }; if number == 2 {
+            canvas.lineColor = Color(hue: hue, saturation: 80, brightness: brightness, alpha: 100)
+            canvas.drawLine(fromX: c, fromY: y, toX: c+3, toY: c+4)
         }
         else { canvas.drawRectangle(centreX: c, centreY: y, width: 20, height: 20)
         }}}
